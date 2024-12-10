1fc8b2e06e674a3ac245968666cee70f97a7ebd5
Ismael Kentenich <ismaelkentenichm@gmail.com>
2024-12-10 20:39:36 -0300

    feat: create Button component

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,12 +1,17 @@
+import { Button } from "@/components/button";
 import { Steps } from "@/components/steps";
 import { Welcome } from "@/components/welcome";
 import { View, Text } from "react-native";
+import { IconPlus } from "@tabler/icons-react-native";
 
 export default function Index() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
       <Steps />
+      <Button>
+        <Button.Title>Começar</Button.Title>
+      </Button>
     </View>
   );
 }
