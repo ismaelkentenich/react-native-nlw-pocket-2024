567f814cd5ea7bf7fe98b65ea3cbba847ff4aa92
Ismael Kentenich <ismaelkentenichm@gmail.com>
2024-12-10 22:24:29 -0300

    feat: render places from API

--- mobile/src/app/home.tsx
+++ mobile/src/app/home.tsx
@@ -3,10 +3,15 @@ import { Alert, Text, View } from "react-native";
 
 import { api } from "@/services/api";
 import { Categories, CategoryProps } from "@/components/categories";
+import { PlaceProps } from "@/components/place";
+import { Places } from "@/components/places";
+
+type MarketsProps = PlaceProps & {};
 
 export default function Home() {
   const [categories, setCategories] = useState<CategoryProps>([]);
   const [category, setCategory] = useState("");
+  const [markets, setMarkets] = useState<MarketsProps[]>([]);
 
   async function fetchCategories() {
     try {
@@ -19,9 +24,26 @@ export default function Home() {
     }
   }
 
+  async function fetchMarkets() {
+    try {
+      if (!category) {
+        return;
+      }
+      const { data } = await api.get("/markets/category/" + category);
+      setMarkets(data);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Locais", "Não foi possível carregar os locais!");
+    }
+  }
+
   useEffect(() => {
     fetchCategories();
   }, []);
+
+  useEffect(() => {
+    fetchMarkets();
+  }, [category]);
 
   return (
     <View style={{ flex: 1 }}>
@@ -30,6 +52,7 @@ export default function Home() {
         onSelect={setCategory}
         selected={category}
       />
+      <Places data={markets} />
     </View>
   );
 }
