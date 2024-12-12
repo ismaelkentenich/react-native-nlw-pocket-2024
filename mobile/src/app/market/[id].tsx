d74232942e5c33d9bd72fa4089daead40e76f51a
Ismael Kentenich <ismaelkentenichm@gmail.com>
2024-12-11 22:07:27 -0300

    feat: fetch market data on market screen

--- mobile/src/app/market/[id].tsx
+++ mobile/src/app/market/[id].tsx
@@ -1,8 +1,37 @@
-import { View, Text } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { View, Text, Alert } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+
+import { Loading } from "@/components/loading";
+
+import { api } from "@/services/api";
 
 export default function Market() {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
   const params = useLocalSearchParams<{ id: string }>();
+
+  async function fetchMarket() {
+    try {
+      const { data } = await api.get("/markets/" + params.id);
+      setData(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Erro", "Não foi possível carregar os dados.", [
+        { text: "Ok", onPress: () => router.back() },
+      ]);
+    }
+  }
+
+  useEffect(() => {
+    fetchMarket();
+  }, [params.id]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
