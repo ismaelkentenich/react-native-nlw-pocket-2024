7652585667b49598cc70b25a54213ab4235aa7ea
Ismael Kentenich <ismaelkentenichm@gmail.com>
2024-12-12 21:53:24 -0300

    feat: add market screen

--- mobile/src/app/market/[id].tsx
+++ mobile/src/app/market/[id].tsx
@@ -1,15 +1,27 @@
 import { useEffect, useState } from "react";
-import { View, Text, Alert } from "react-native";
-import { router, useLocalSearchParams } from "expo-router";
+import { View, Alert, Modal } from "react-native";
+import { router, useLocalSearchParams, Redirect } from "expo-router";
+import { useCameraPermissions, CameraView } from "expo-camera";
 
 import { Loading } from "@/components/loading";
+import { Cover } from "@/components/market/cover";
+import { Details, PropsDetails } from "@/components/market/details";
 
 import { api } from "@/services/api";
+import { Coupon } from "@/components/market/coupon";
+import { Button } from "@/components/button";
+
+type DataProps = PropsDetails & {
+  cover: string;
+};
 
 export default function Market() {
-  const [data, setData] = useState();
+  const [data, setData] = useState<DataProps>();
   const [isLoading, setIsLoading] = useState(true);
+  const [coupon, setCoupon] = useState<string | null>(null);
+  const [isVisibleCameraModal, setIsVisibleCameraModal] = useState(false);
 
+  const [_, requestPermission] = useCameraPermissions();
   const params = useLocalSearchParams<{ id: string }>();
 
   async function fetchMarket() {
@@ -25,6 +37,21 @@ export default function Market() {
     }
   }
 
+  async function handleOpenCamera() {
+    try {
+      const { granted } = await requestPermission();
+
+      if (!granted) {
+        return Alert.alert("Câmera", "Você precisa habilitar o uso da câmera.");
+      }
+
+      setIsVisibleCameraModal(true);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Câmera", "Não foi possível utilizar a câmera.");
+    }
+  }
+
   useEffect(() => {
     fetchMarket();
   }, [params.id]);
@@ -33,9 +60,30 @@ export default function Market() {
     return <Loading />;
   }
 
+  if (!data) {
+    return <Redirect href="/home" />;
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>{params.id}</Text>
+    <View style={{ flex: 1 }}>
+      <Cover uri={data.cover} />
+      <Details data={data} />
+      {coupon && <Coupon code="CUPOM2024" />}
+
+      <View style={{ padding: 32 }}>
+        <Button onPress={handleOpenCamera}>
+          <Button.Title>Ler QR Code</Button.Title>
+        </Button>
+      </View>
+
+      <Modal style={{ flex: 1 }} visible={isVisibleCameraModal}>
+        <CameraView style={{ flex: 1 }} />
+        <View style={{ position: "absolute", bottom: 32, left: 32, right: 32 }}>
+          <Button onPress={() => setIsVisibleCameraModal(false)}>
+            <Button.Title>Voltar</Button.Title>
+          </Button>
+        </View>
+      </Modal>
     </View>
   );
 }
